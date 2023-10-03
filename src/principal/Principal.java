53b4934cd69f396038a942243f17aeb1c8843e91
Mario Wilson Garcia <70682960+gwm1965@users.noreply.github.com>
2023-10-03 03:57:56 -0300

    Projeto completo

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,48 +1,97 @@
+package principal;
+
 import javax.swing.*;
 
 
+import ConversorTemperatura.FuncaoTemperatura;
+import ConversorVelocidade.FuncaoVelocidades;
+import conversorMoedas.Funcao;
 
 
 public class Principal {
     public static void main(String[] args) {
 
         Funcao moedas = new Funcao();
+        FuncaoTemperatura temperatura = new FuncaoTemperatura();
+        FuncaoVelocidades velocidades = new FuncaoVelocidades();
 
-        String opcao = (JOptionPane.showInputDialog(null,"Escolha a moeda para a qual deseja converter seu dinheiro", "Moedas", JOptionPane.PLAIN_MESSAGE, null,new Object[]{"De Reais a Dolares","De Reais a Euros", "De Dolares a Reais","De Reais a Euros", "De Euros a Reais"},"Escolha")).toString();
+        while (true) {
 
+            String opcao = (JOptionPane.showInputDialog(null,"Escolha uma opção", "Menu", JOptionPane.PLAIN_MESSAGE, null,new Object[]{"Converasor de Moeda","Conversor de Temperatura", "Conversor de Velocidade"} ,"Escolha")).toString();
 
+            switch (opcao) {
+                case "Converasor de Moeda":
+                    String input = JOptionPane.showInputDialog("Insira um valor: ");
+                    if (checar(input)) {
+                        double valorRecebido = Double.parseDouble(input);
+                        moedas.converterMoeda(valorRecebido);
 
-
-
-        switch (opcao) {
-            case "Conversor de Moeda":
-                String input = JOptionPane.showInputDialog("Insira um valor: ");
-                if (checar(input)) {
-                    double valorRecebido = Double.parseDouble(input);
-                    moedas.converterMoeda(valorRecebido);
-
-                    int resposta = JOptionPane.showConfirmDialog(null, "Deseja continuar?");
-                    if (JOptionPane.OK_OPTION == resposta) {
-                        System.out.println("Escolha opção Afirmativa");
+                        int resposta = JOptionPane.showConfirmDialog(null, "Deseja continuar?");
+                        if (JOptionPane.OK_OPTION == resposta) {
+                            System.out.println("Escolha opção Afirmativa");
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Programa finalizado");
+                        }
                     } else {
-                        JOptionPane.showMessageDialog(null, "Programa finalizado");
+                        JOptionPane.showMessageDialog(null, "Valor inválido");
                     }
-                } else {
-                    JOptionPane.showMessageDialog(null, "Valor inválido");
-                }
-                break;
+                    break;
 
+                case "Conversor de Temperatura":
+                    input = JOptionPane.showInputDialog("Insira um Valor de Temperatura para Converter");
+                    if (checar(input)) {
+                        double valorRecebido = Double.parseDouble(input);
+                        temperatura.converterTemperatura(valorRecebido);
+
+                        int resposta = 0;
+                        resposta = JOptionPane.showConfirmDialog(null, "Deseja continuar?");
+                        if ((resposta == 0) && (checar(input))) {
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Programa finalizado");
+                        }
+
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Valor inválido");
+                    }
+                    break;
+
+                case "Conversor de Velocidade":
+                    input = JOptionPane.showInputDialog("Qual a velocidade ou distancia para converter");
+                    if (checar(input)) {
+                        double valorRecebido = Double.parseDouble(input);
+                        velocidades.converterVelocidades(valorRecebido);
+
+                        int resposta = 0;
+                        resposta = JOptionPane.showConfirmDialog(null, "Deseja continuar?");
+                        if ((resposta == 0) && (checar(input))) {
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Programa finalizado");
+                        }
+
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Valor inválido");
+                    }
+                    break;
+
+
+
+            }
         }
+
     }
 
     public static boolean checar(String input) {
         try {
-            double x Double.parseDouble(input);
+            double x = Double.parseDouble(input);
             if (x >= 0 || x < 0) ;
             return true;
         } catch (NumberFormatException e) {
             return false;
         }
     }
+
 }
+
+
+
 
